4efc4983b34ae9d956e0dfac8adbf2d8040348a3
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2020-05-19 16:17:37 +0100

    wrap the collection

--- DIAGRAM.md
+++ DIAGRAM.md
@@ -6,8 +6,8 @@ Interaction diagram that shows how Objects use Messages to communicate with one 
 Bike <-- working? --> true/false
 DockingStation <-- release_bike --> a Bike(if available)
 DockingStation <-- release_bike --> error(if none available)
-DockingStation <-- dock(a Bike) --> a Bike(if capacity)
-DockingStation <-- dock(a Bike) --> error(if no capacity)
+DockingStation <-- dock(a Bike) --> [a Bike](if capacity)
+DockingStation <-- dock(a Bike) --> error(if out of capacity)
 DockingStation <-- bike --> a Bike
 ```
 Back to [readme](README.md).
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,32 +3,24 @@ require 'docking_station'
 describe DockingStation do
 
   bike = Bike.new
-  bike2 = Bike.new
 
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
     it 'releases a bike' do
-      subject.dock(bike)
+      subject.dock bike
       expect(subject.release_bike).to eq bike
     end
   end
 
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(bike)
-      expect { subject.dock bike2 }.to raise_error 'Docking station full'
+      20.times { subject.dock bike }
+      expect { subject.dock bike }.to raise_error 'Docking station full'
     end
     it 'docks objects' do
-      expect(subject.dock(bike)).to eq bike
-    end
-  end
-
-  describe '#bike' do
-    it 'returns docked bikes' do
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.dock bike).to eq [bike]
     end
   end
 
